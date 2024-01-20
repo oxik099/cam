e0d15d6a69193617c4882a3f379f1c4963f63815
Oksana Dotsenko <127159256+oxik099@users.noreply.github.com>
2024-01-20 20:42:26 +0000

    search icon
    added a function for a search icon in nav bar

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 
+const searchInput = document.querySelector(".search-input");
+const searchIcon = document.querySelector("#search-icon")
+
+
+searchIcon.addEventListener('click', () => {
+  if (searchInput.style.display === "none" || searchInput.style.display === "") {
+    searchInput.style.display = "block";
+    searchInput.classList.add(".smooth-transition");
+  } else {
+    searchInput.style.display = "none";
+  }
+});
+
+
 
 var mainSlider = new Swiper(".mySwiper2", {
 	parallax: true,
