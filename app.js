c4acc46b5b19f95859afbb2c8e25fbad7a430040
Oksana Dotsenko <127159256+oxik099@users.noreply.github.com>
2024-01-27 15:32:30 +0000

    CSS changes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-const searchInput = document.querySelector(".search-input");
-const searchIcon = document.querySelector("#search-icon")
+// const searchInput = document.querySelector(".search-input");
+// const searchIcon = document.querySelector("#search-icon")
 
 
-searchIcon.addEventListener('click', () => {
-  if (searchInput.style.display === "none" || searchInput.style.display === "") {
-    searchInput.style.display = "block";
-    searchInput.classList.add(".smooth-transition");
-  } else {
-    searchInput.style.display = "none";
-  }
-});
+// searchIcon.addEventListener('click', () => {
+//   if (searchInput.style.display === "none" || searchInput.style.display === "") {
+//     searchInput.style.display = "block";
+//     searchInput.classList.add(".smooth-transition");
+//   } else {
+//     searchInput.style.display = "none";
+//   }
+// });
 
 
 
